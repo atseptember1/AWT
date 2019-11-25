c6419a31936bb89f12c8801d27d986934d964523
Hai Anh Bui <haian1@iMac.local>
2019-11-25 17:16:51 +0100

    add logging, updata ffmpeg command, change some variable name e.g: video_folders to videos...

--- 03_extract_metrics_logs.py
+++ 03_extract_metrics_logs.py
@@ -1,33 +1,33 @@
 import os
 import subprocess
 import glob
+import logging
 
+logging.basicConfig(filename='extract_metrics.log', level=logging.DEBUG, 
+                    format='%(asctime)s: %(message)s')
 
 resolutions = ['768x432','960x540','1280x720']
+algorithms = ["fast_bilinear", "bilinear", "bicubic", "experimental", "neighbor", "area", "bicublin", "gauss", "sinc", "lanczos", "spline"]
 log_files_path = os.path.join(os.getcwd(), "log_files")
 ref_vid_path = os.path.join(os.getcwd(),"reference_videos")
-video_folders = list(filter(lambda x: x[0] != '.',os.listdir(ref_vid_path)))
+videos = list(filter(lambda x: x[0] != '.',os.listdir(ref_vid_path)))
 
-for video_folder in video_folders:
+for video in videos:
 
-    folder_path = os.path.join(ref_vid_path, video_folder)
+    video_dir = os.path.join(ref_vid_path, video)
     
-    ref = glob.glob(os.path.join(folder_path, '*SPL*'))[0]
-    #print(ref)
+    ref = os.path.join(video_dir, "1_cut_SPL_" + video + ".mov")
+    logging.debug("REFERENCE VIDEO:{}".format(ref))
     for resolution in resolutions:
-        upscale_videos = glob.glob(os.path.join(folder_path,resolution + '*' + 'bilinear_new' + "*"))
+            for alg in algorithms:
+                upscale_video = os.path.join(video_dir, "_".join((resolution, alg, "new")) + '.mp4')
+                logging.debug("UPSCALE VIDEO:{}".format(upscale_video))
 
-        for main in upscale_videos:
-            tmp = main.split("_")
-            if 'fast' in tmp:
-                name_log = video_folder + '_' + resolution + '_' 'fast' + '_' + tmp[-2] + '.log'
-            else:
-                name_log = video_folder + '_' + resolution + '_' + tmp[-2] + '.log'
-            log_path = os.path.join(log_files_path, name_log)
-            print(log_path)
-            subprocess.call(['ffmpeg -i {} -i {} -lavfi "libvmaf=psnr=1:log_fmt=json:log_path={}" -f null -'.format(main, ref, log_path)], shell=True)
-
-
+                name_log = "_".join((video, resolution, alg)) + '.log'
+                log_path = os.path.join(log_files_path, name_log)
+                
+                logging.debug('ffmpeg -i {} -i {} -lavfi "[0:v]settb=AVTB,setpts=PTS-STARTPTS[main];[1:v]settb=AVTB,setpts=PTS-STARTPTS[ref];[main][ref]libvmaf=psnr=1:log_fmt=json:log_path={}" -f null -'.format(upscale_video, ref, log_path))
+                subprocess.call(['ffmpeg -i {} -i {} -lavfi "[0:v]settb=AVTB,setpts=PTS-STARTPTS[main];[1:v]settb=AVTB,setpts=PTS-STARTPTS[ref];[main][ref]libvmaf=psnr=1:log_fmt=json:log_path={}" -f null -'.format(upscale_video, ref, log_path)], shell=True)
 
 
 
