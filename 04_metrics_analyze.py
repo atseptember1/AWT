126ea17e201f28f19381659a101b177e205eb05e
Anh Tu Nguyen <anhtunguyen1809@hotmail.com>
2019-11-26 02:17:13 +0100

    add 768x432 to resolutions

--- 04_metrics_analyze.py
+++ 04_metrics_analyze.py
@@ -9,7 +9,7 @@ ref_vid_path = os.path.join(os.getcwd(), 'reference_videos')
 videos = list(filter(lambda x: x[0] != '.',os.listdir(ref_vid_path)))
 log_files_path = os.path.join(os.getcwd(), "log_files")
 
-resolutions = ['960x540','1280x720']
+resolutions = ['768x432','960x540','1280x720']
 algorithms = ["fast_bilinear", "bilinear", "bicubic", "experimental", "neighbor", "area", "bicublin", "gauss", "sinc", "lanczos", "spline"]
 
 times = {}
